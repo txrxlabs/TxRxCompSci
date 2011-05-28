2f385cdcd670b9073160bc19787f0958efde4aa7
Patrick Wheeler <Patrick.John.Wheeler@gmail.com>
2011-05-28 18:54:54 -0500

    added set function to closure example to show that scope is kept around

--- scheme/examples/closure example.rkt
+++ scheme/examples/closure example.rkt
@@ -1,12 +1,13 @@
+(define first car)
+(define second cadr)
+
 (define (plus-var var)
   (lambda (x) (+ var x)))
 
 (define plus-one (plus-var 1))
 
-(print "(plus-one 1)")
 (plus-one 1)
 
-(print "(plus-one 2)")
 (plus-one 2)
 
 (define plus-two (plus-var 2))
@@ -15,13 +16,21 @@
 
 (plus-two 2)
 
+(define id (lambda (x) x))
 
 (define (make-obj lst)
   (lambda (getter)
          (getter lst)))
 
 (define tmp-obj (make-obj '(1 2 3)))
+(define (obj-set-car x)
+  (lambda (lst)
+    (set-car! lst x)))
 
 (tmp-obj first)
+;1
 
-(tmp-obj second)
+(tmp-obj (obj-set-car 2))
+
+(tmp-obj first)
+;2
